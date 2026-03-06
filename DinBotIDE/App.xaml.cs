de60bf59e7c6a016c6c76a0dd90d8059dd044b76
Juan Nuñez <86734411+Juanzett@users.noreply.github.com>
2026-03-06 18:51:51 -0300

    Add App.xaml.cs file for application initialization

---
+++ DinBotIDE/App.xaml.cs
@@ -0,0 +1,13 @@
+using System.Windows;
+
+namespace DinBotIDE
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            // Inicialización global del IDE
+        }
+    }
+}
